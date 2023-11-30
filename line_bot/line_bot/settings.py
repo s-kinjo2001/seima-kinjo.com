fadb937fa5e4fc4b238d386294420cd383e06aa1
Seima Kinjo <s.kinjo@asup.co.jp>
2023-11-30 18:48:56 +0900

    loggingの設定

--- line_bot/line_bot/settings.py
+++ line_bot/line_bot/settings.py
@@ -96,3 +96,65 @@ STATIC_ROOT = '/var/www/seima-kinjo.com/html/line_bot/static'
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+             'format': ('%(levelname)s [%(asctime)s] %(name)s %(message)s'),
+        },
+         'verbose': {
+             'format': '%(levelname)s %(asctime)s %(name)s %(process)d %(thread)d %(message)s'
+         },
+     },
+     'filters': {
+         'require_debug_false': {
+             '()': 'django.utils.log.RequireDebugFalse'
+         },
+         'require_debug_true': {
+             '()': 'django.utils.log.RequireDebugTrue'
+         },
+     },
+     'handlers': {
+         'debug-console': {
+             'level': 'ERROR',
+             'filters': ['require_debug_true'],  # settings.DEBUG=Falseなら全て破棄
+             'class': 'logging.StreamHandler',
+             'formatter': 'verbose'
+         },
+         'prod-console': {
+             'level': 'ERROR',
+             'filters': ['require_debug_false'],  # settings.DEBUG=Trueなら全て破棄
+             'class': 'logging.StreamHandler',
+             'formatter': 'standard'
+         },
+         'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'log/debug.log'),
+            'formatter': 'verbose',
+        },
+     },
+     'loggers': {
+         '': {  # 'root' の代わり。全てキャッチする
+             'handlers': ['prod-console', 'debug-console'],
+             'level': 'NOTSET',
+             'propagate': False
+         },
+         'django': {
+             'handlers': ['prod-console', 'debug-console'],
+             'level': 'ERROR',  # Djangoモジュール由来のログをERROR以上のみに制限
+             'propagate': False
+         },
+         'django.request': {
+             'handlers': ['prod-console', 'debug-console'],
+             'level': 'ERROR',  # Djangoモジュール由来のログをERROR以上のみに制限
+             'propagate': False,
+         },
+         'django.db.backends': {
+             'handlers': ['prod-console', 'debug-console'],
+             'level': 'DEBUG',  # DBに発行するSQLログを出力（実際の出力はhandlerの方で制御する）
+             'propagate': False
+         },
+     }
+ }
