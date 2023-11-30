d59cf85c4603d7609d971f6843701b1ffdee563a
Seima Kinjo <s.kinjo@asup.co.jp>
2023-11-30 16:50:02 +0900

    app作成

---
+++ line_bot/bot/line_message.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import urllib.request
+import json
+import environ
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(Path.joinpath(BASE_DIR, '.env'))
+
+REPLY_ENDPOINT_URL = "https://api.line.me/v2/bot/message/reply"
+ACCESSTOKEN = env('ACCESSTOKEN')
+HEADER = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + ACCESSTOKEN
+}
+
+class LineMessage():
+    def __init__(self, messages):
+        self.messages = messages
+
+    def reply(self, reply_token):
+        body = {
+            'replyToken': reply_token,
+            'messages': self.messages
+        }
+        print(body)
+        req = urllib.request.Request(REPLY_ENDPOINT_URL, json.dumps(body).encode(), HEADER)
+        try:
+            with urllib.request.urlopen(req) as res:
+                body = res.read()
+        except urllib.error.HTTPError as err:
+            print(err)
+        except urllib.error.URLError as err:
+            print(err.reason)
--- line_bot/line_bot/settings.py
+++ line_bot/line_bot/settings.py
@@ -20,6 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'utils',
+    'bot',
 ]
 
 MIDDLEWARE = [
